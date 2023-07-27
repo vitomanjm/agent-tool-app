d06f67637e8a3e285ee66f9c9607279a45a3a503
vitomanjm <vitomanjm@gmail.com>
2023-07-27 00:21:55 -0500

    Eighth commit Agent tool - Hours Calculator and To do list fixes

--- src/components/HoursCalcInput.vue
+++ src/components/HoursCalcInput.vue
@@ -36,24 +36,19 @@ export default {
         },
 
         calculateHours() {
-            if (this.startTime && this.endTime) {
-                const start = this.parseTime(this.startTime);
-                const end = this.parseTime(this.endTime);
 
-                if (isNaN(start) || isNaN(end)) {
-                    this.hoursResult = 'Invalid time format';
-                } else {
-                    const millisecondsDifference = differenceInMilliseconds(end, start);
-                    const hours = Math.floor(millisecondsDifference / (1000 * 60 * 60));
-                    const minutes = Math.floor((millisecondsDifference % (1000 * 60 * 60)) / (1000 * 60));
-                    const seconds = Math.floor((millisecondsDifference % (1000 * 60)) / 1000);
+            const start = this.parseTime(this.startTime);
+            const end = this.parseTime(this.endTime);
+            const millisecondsDifference = differenceInMilliseconds(end, start);
+            const hours = Math.floor(millisecondsDifference / (1000 * 60 * 60));
+            const minutes = Math.floor((millisecondsDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((millisecondsDifference % (1000 * 60)) / 1000);
 
-                    this.hoursResult = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-                }
-            } else {
-                this.hoursResult = '';
-            }
-        },
+            this.hoursResult = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }
+
+
+
 
     },
 
